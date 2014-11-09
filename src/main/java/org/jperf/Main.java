59a6e6d673bb14750de923a2a81c4df528fd9f97
Andy Grove <andy.grove@codefutures.com>
2014-11-09 09:46:42 -0700

    convert to maven

--- src/main/java/org/jperf/Main.java
+++ src/main/java/org/jperf/Main.java
@@ -1,8 +1,8 @@
 package org.jperf;
 
-import org.jdom.Document;
-import org.jdom.Element;
-import org.jdom.input.SAXBuilder;
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.input.SAXBuilder;
 
 import java.io.FileInputStream;
 import java.lang.reflect.Constructor;
