adb1aba4a4318718c0e6175994c842e2b9d0fc0b
Andy Grove <andygrove73@gmail.com>
2014-12-03 15:21:24 -0800

    Add support for writing csv to file

--- src/main/java/org/jperf/ResultWriterCSV.java
+++ src/main/java/org/jperf/ResultWriterCSV.java
@@ -1,27 +1,59 @@
 package org.jperf;
 
+import com.google.common.base.Throwables;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.text.DecimalFormat;
 
 public class ResultWriterCSV implements ResultWriter {
 
   final DecimalFormat formatter = new DecimalFormat("0");
 
+  private FileOutputStream os;
+
+  public ResultWriterCSV() {
+  }
+
+  public ResultWriterCSV(File file) throws IOException {
+    System.out.println("Writing results to " + file.getAbsolutePath());
+    this.os = new FileOutputStream(file);
+  }
+
   @Override
   public void writeHeader(PerfTestConfig config) throws Exception {
-    System.out.println("Threads,Samples,Duration,Throughput");
+    String header = "Threads,Samples,Duration,Throughput";
+    System.out.println(header);
+    if (os != null) {
+      os.write((header+"\n").getBytes());
+      os.flush();
+    }
   }
 
   @Override
   public void writeResult(int threadCount, long duration, long samples) throws Exception {
-    System.out.println(String.format("%s,%s,%s,%s",
+    String line = String.format("%s,%s,%s,%s",
         formatter.format(threadCount),
         formatter.format(samples),
         formatter.format(duration),
         formatter.format(samples * 1000.0f / duration)
-    ));
+    );
+    System.out.println(line);
+    if (os != null) {
+      os.write((line + "\n").getBytes());
+      os.flush();
+    }
   }
 
   @Override
   public void close() {
+    if (os != null) {
+      try {
+        os.close();
+      } catch (IOException e) {
+        throw Throwables.propagate(e);
+      }
+    }
   }
 }
